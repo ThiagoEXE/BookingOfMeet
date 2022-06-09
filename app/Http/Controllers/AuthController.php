02fa37f6fd513de7e00b359b102b6ea386308a3d
ThiagoEXE <thiagob.oliveira@ucsal.edu.br>
2022-06-09 13:09:39 -0300

    criando autenticação do usuário

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -5,11 +5,10 @@ use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
 
-class UserController extends Controller
+class AuthController extends Controller
 {
     //Registrar um novo usuário
     public function register(Request $request){
-
         $request->validate([
             //'id' => 'incre'
             'name' => 'required|string',
@@ -22,10 +21,8 @@ class UserController extends Controller
              'email' => $request->email,
              'password' => bcrypt($request->password)
     ]);
-
     //Token de autenticação
         $token = $user->createToken('newtoken')->plainTextToken;
-
         $response = [
             'user'=> $user,
             'token' => $token
@@ -35,4 +32,23 @@ class UserController extends Controller
 
   }
 
+    //Login do Usuário
+        public function login(Request $request){
+           $request->validate([
+
+            'email' => 'required|string|',
+            'password' =>'required|string'
+        ]);
+
+        //checar se o email do usuário existe no banco de dados
+        $user = User::where('email', $request->email)->first();
+
+        //valida o usuário e checa a senha
+
+        if(!$user || !Hash::check($request->password, $user->password)){
+            return response([
+                'message' => 'Invalid Credentials'
+            ], 401);
+        }
+   }
 }
