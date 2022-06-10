88f7963b0ca14476a7b713adb972e223bf1beb28
CandidoPeralva <niantic68@hotmail.com>
2022-06-10 09:12:42 -0300

    ignore

--- resources/views/indexlogin.blade.php
+++ resources/views/indexlogin.blade.php
@@ -13,7 +13,7 @@
       href="https://fonts.googleapis.com/css2?family=Nunito+Sans:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400;1,700&family=Open+Sans:wght@300&display=swap"
       rel="stylesheet"
     />
-    <link rel="stylesheet" href="../css/style.css" />
+    <link rel="stylesheet" href="{{ asset('css/stylelogin.css') }}" />
     <meta charset="UTF-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
@@ -51,6 +51,7 @@
                       <label class="email" for="email ">Type your e-mail / User</label>
                       <input
                         type="email"
+                        name="email"
                         id="email"
                         class="form-control mt-2"
                         placeholder="Type your e-mail "
@@ -64,6 +65,7 @@
                     <label for="pass">Type your password</label>
                     <input
                       type="password"
+                      name="password"
                       id="pass"
                       class="form-control mt-2"
                       placeholder="Type your password"
